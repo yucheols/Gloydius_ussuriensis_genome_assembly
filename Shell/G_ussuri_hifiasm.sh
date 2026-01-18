54c846ea08f9806d16fbf6ce16c0be90f0ab8130
Yucheol Shin <yshin@amnh.org>
2026-01-18 00:03:58 -0500

    commit shell script changes

--- Shell/G_ussuri_hifiasm.sh
+++ Shell/G_ussuri_hifiasm.sh
@@ -3,16 +3,16 @@
 #SBATCH --nodes=1
 #SBATCH --mem=650G
 #SBATCH --partition=bigmem
-#SBATCH --cpus-per-task=48
+#SBATCH --cpus-per-task=24
 #SBATCH --time=7-00:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=yshin@amnh.org
-#SBATCH --output=/home/yshin/mendel-nas1/snake_genome_ass/G_ussuriensis_Chromo/PacBio_Revio/outfiles/slurm-%j_%x.out
-#SBATCH --error=/home/yshin/mendel-nas1/snake_genome_ass/G_ussuriensis_Chromo/PacBio_Revio/outfiles/slurm-%j_%x.err
+#SBATCH --output=/home/yshin/mendel-nas1/snake_genome_ass/G_ussuriensis_Chromo/PacBio_Revio/outfiles/slurm-%x_%j.out
+#SBATCH --error=/home/yshin/mendel-nas1/snake_genome_ass/G_ussuriensis_Chromo/PacBio_Revio/outfiles/slurm-%x_%j.err
 
 # initiate conda and activate the conda environment
 source ~/.bash_profile
-conda activate mytools
+conda activate genome_assembly
 
 # set taxon name as a variable
 name="Gloydius_ussuriensis"
