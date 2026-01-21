7ee44b48ccfb778c139b3aa1076373f3db5058e2
Yucheol Shin <yshin@amnh.org>
2026-01-21 16:12:53 -0500

    commit

--- Shell/G_ussuri_busco.sh
+++ Shell/G_ussuri_busco.sh
@@ -5,6 +5,7 @@
 #SBATCH --partition=compute
 #SBATCH --cpus-per-task=24
 #SBATCH --time=12:00:00
+#SBATCH --mail-type=ALL
 #SBATCH --mail-user=yshin@amnh.org
 #SBATCH --output=/home/yshin/mendel-nas1/snake_genome_ass/G_ussuriensis_Chromo/PacBio_Revio/outfiles/slurm-%x_%j.out
 #SBATCH --error=/home/yshin/mendel-nas1/snake_genome_ass/G_ussuriensis_Chromo/PacBio_Revio/outfiles/slurm-%x_%j.err
