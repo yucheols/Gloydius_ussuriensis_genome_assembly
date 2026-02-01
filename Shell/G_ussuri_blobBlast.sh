b5c855002294a25e9533386c03a260515da925df
Yucheol Shin <yshin@amnh.org>
2026-01-31 19:03:59 -0500

    commit

--- Shell/G_ussuri_blobBlast.sh
+++ Shell/G_ussuri_blobBlast.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 #SBATCH --job-name=blobBlst_ussuri
 #SBATCH --nodes=1
-#SBATCH --mem=200G
-#SBATCH --partition=compute
+#SBATCH --mem=500G
+#SBATCH --partition=bigmem
 #SBATCH --cpus-per-task=32
 #SBATCH --time=72:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=yshin@amnh.org
-#SBATCH --output=/home/yshin/mendel-nas1/snake_genome_ass/G_ussuriensis_Chromo/PacBio_Revio/outfiles/slurm-%x_%j.out
-#SBATCH --error=/home/yshin/mendel-nas1/snake_genome_ass/G_ussuriensis_Chromo/PacBio_Revio/outfiles/slurm-%x_%j.err
+#SBATCH --output=/home/yshin/nas4/G_ussuriensis_Chromo/slurm_logs/slurm-%x_%j.out
+#SBATCH --error=/home/yshin/nas4/G_ussuriensis_Chromo/slurm_logs/slurm-%x_%j.err
 
+# this is run on the AMNH Huxley cluster
 # load module
-module load NCBI/blast-2.16.0+
+module load BLAST/2.11.0-Linux_x86_64 
 
 # path to genome cleanup directory
-wdir=/home/yshin/mendel-nas1/snake_genome_ass/G_ussuriensis_Chromo/genome_cleanup
+wdir=/home/yshin/nas4/G_ussuriensis_Chromo/genome_cleanup
+
+# path to DB
+export BLASTDB=/nas3/database/nt_2024_10_23
+blastdbcmd -db nt -info
+
+# path to assembly
+path_to_asm=${wdir}/Gloydius_ussuriensis_v1.asm.bp.p_ctg.fa
+
+# output dir
+out_dir=/home/yshin/nas4/G_ussuriensis_Chromo/genome_cleanup/blast_out
 
 # run blast
 blastn \
-  -task megablast
+  -db nt \
+  -task megablast \
+  -query ${path_to_asm} \
+  -out ${out_dir}/ussuri_megablast.nt \
+  -evalue 1e-5 \
+  -outfmt "6 qseqid staxids bitscore sgi sskingdoms sscinames" \
+  -max_target_seqs 1 \
+  -num_threads=${SLURM_CPUS_PER_TASK}
+
+  
   
--- doc.md
+++ doc.md
@@ -308,6 +308,49 @@ tar zxf data/taxdump.tar.gz -C data/ nodes.dmp names.dmp
 ${blobdir}/blobtools nodesdb --nodes data/nodes.dmp --names data/names.dmp
 
 ```
+To generate the blast hit file, we will use the Huxley cluster because it already has the database soft linked to /nas3. Blast is also available as a module on Huxley.
+
+```sh
+#!/bin/bash
+#SBATCH --job-name=blobBlst_ussuri
+#SBATCH --nodes=1
+#SBATCH --mem=500G
+#SBATCH --partition=bigmem
+#SBATCH --cpus-per-task=32
+#SBATCH --time=72:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yshin@amnh.org
+#SBATCH --output=/home/yshin/nas4/G_ussuriensis_Chromo/slurm_logs/slurm-%x_%j.out
+#SBATCH --error=/home/yshin/nas4/G_ussuriensis_Chromo/slurm_logs/slurm-%x_%j.err
+
+# this is run on the AMNH Huxley cluster
+# load module
+module load BLAST/2.11.0-Linux_x86_64 
+
+# path to genome cleanup directory
+wdir=/home/yshin/nas4/G_ussuriensis_Chromo/genome_cleanup
+
+# path to DB
+export BLASTDB=/nas3/database/nt_2024_10_23
+blastdbcmd -db nt -info
+
+# path to assembly
+path_to_asm=${wdir}/Gloydius_ussuriensis_v1.asm.bp.p_ctg.fa
+
+# output dir
+out_dir=/home/yshin/nas4/G_ussuriensis_Chromo/genome_cleanup/blast_out
+
+# run blast
+blastn \
+  -db nt \
+  -task megablast \
+  -query ${path_to_asm} \
+  -out ${out_dir}/ussuri_megablast.nt \
+  -evalue 1e-5 \
+  -outfmt "6 qseqid staxids bitscore sgi sskingdoms sscinames" \
+  -max_target_seqs 1 \
+  -num_threads=${SLURM_CPUS_PER_TASK}
+```
 
 
 ## 7) Scaffolding through Hi-C data incorporation
