2d54eb60710952f800492f49e3bae1d772e388ca
Yucheol Shin <yshin@amnh.org>
2026-02-28 18:05:44 -0500

    commit

--- Shell/G_ussuri_hisat2_preHiC.sh
+++ Shell/G_ussuri_hisat2_preHiC.sh
@@ -16,7 +16,7 @@ export PATH=/home/yshin/mendel-nas1/miniconda3/bin:$PATH
 conda activate hisat2
 
 # path to draft
-draft_path=/home/yshin/mendel-nas1/snake_genome_ass/G_ussuriensis_Chromo/PacBio_Revio/no_mito/Gloydius_ussuriensis_AMNH_21010_noMito.fa
+draft_path=/home/yshin/mendel-nas1/snake_genome_ass/G_ussuriensis_Chromo/PacBio_Revio/no_mito
 
 # path to RNAseq reads
 rna_path=/home/yshin/mendel-nas1/snake_genome_ass/G_ussuriensis_Chromo/annotation/paired_RNAseq_reads
@@ -26,12 +26,22 @@ outpath=/home/yshin/mendel-nas1/snake_genome_ass/G_ussuriensis_Chromo/annotation
 mkdir -p ${outpath}
 
 # build a genome index
-hisat2-build ${draft_path}
+hisat2-build ${draft_path}/Gloydius_ussuriensis_AMNH_21010_noMito.fa ${outpath}/Gloydius_ussuriensis_AMNH_21010_noMito
 
-# map RNA reads to the reference genome using HiSat2
-hisat2 -p ${SLURM_CPUS_PER_TASK} -x genome/HelSar1.2 \
--1 ${rna_path}/R1_paired.fq -2 ${rna_path}/R2_paired.fq | \
-samtools sort -o ${outpath}/rnaseq.bam
+# loop over the RNA reads per tissue type and map to the reference genome using HiSat2
+for type in ${rna_path}/*; do
+  if [ -d "$type" ]; then
+    echo "Mapping RNA-seq reads from the following tissue type: $type"
+    hisat2 -p ${SLURM_CPUS_PER_TASK} -x ${outpath}/Gloydius_ussuriensis_AMNH_21010_noMito \
+    -1 ${rna_path}/AMNH_21010_${type}_R1_paired.fastq.gz -2 ${rna_path}/AMNH_21010_${type}_R2_paired.fastq.gz | \
+    samtools sort -o ${outpath}/${type}.bam
+  fi
+done
 
 # index the bam file
 samtools index ${outpath}/rnaseq.bam
+
+# print when done
+echo "[DONE] HISAT2 mapping complete."
+echo "[INFO] BAMs in: ${outpath}/bam"
+echo "[INFO] Logs in: ${outpath}/logs"
