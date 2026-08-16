1cc404d675068811d1b61f76665791d8d42e1a43
Yucheol Shin <brongersmai2@gmail.com>
2026-08-15 19:51:51 -0700

    commit

--- Shell/funannotate_train.sh
+++ Shell/funannotate_train.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=300G
-#SBATCH --time=300:00:00
+#SBATCH --time=120:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=yshin@amnh.org
 #SBATCH --output=/home/yshin/mendel-nas1/snake_genome_ass/G_ussuriensis_Chromo/scripts/outfiles/slurm-%x_%j.out
