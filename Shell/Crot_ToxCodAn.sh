69af1304588e050dc27a46b136df3a0e7c1b17e0
Yucheol Shin <brongersmai2@gmail.com>
2026-09-15 20:34:09 +0900

    commit

---
+++ Shell/Crot_ToxCodAn.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#SBATCH --job-name=Crot_ToxCodAn
+#SBATCH --nodes=1
+#SBATCH --mem=300G
+#SBATCH --cpus-per-task=32
+#SBATCH --partition=compute
+#SBATCH --time=300:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yshin@amnh.org
+#SBATCH --output=/home/yshin/mendel-nas1/snake_genome_ass/G_ussuriensis_Chromo/scripts/outfiles/slurm-%x_%j.out
+#SBATCH --error=/home/yshin/mendel-nas1/snake_genome_ass/G_ussuriensis_Chromo/scripts/outfiles/slurm-%x_%j.err
+
+# activate conda env for mapping
+source /home/yshin/mendel-nas1/miniconda3/etc/profile.d/conda.sh
+conda activate Toxcodan
+set -euo pipefail
+
+# force UTF-8 output in non-interactive SLURM jobs
+# locale available on all standard Linux nodes
+export LANG=C
+export LC_ALL=C
+
+# force Python itself to use UTF-8
+export PYTHONIOENCODING=UTF-8
+export PYTHONUTF8=1
+
+set -euo pipefail
+
+# set toxcodan and signalp paths
+dir_toxcodan="/home/yshin/mendel-nas1/ToxCodAn/bin"
+signalp_dir="/home/yshin/mendel-nas1/signalp-4.1"
+
+# make toxcodan and signalp available in the PATH
+export PATH="${dir_toxcodan}:${signalp_dir}:${PATH}"
+hash -r
+
+# set input paths
+models_dir="/home/yshin/mendel-nas1/ToxCodAn/models"
+
+# output dir for all toxin gene annotation steps
+outdir="/home/yshin/mendel-nas1/snake_genome_ass/G_ussuriensis_Chromo/annotation/toxin_gene_annotation_RNAdata_added"
+mkdir -p ${outdir}
+
+# output dir for ToxCodAn
+mkdir -p ${outdir}/SRR35908235_ToxCodAn
+
+# run ToxCodAn to annotate the transcriptome
+python "${dir_toxcodan}/toxcodan.py" \
+    -s SRR35908235 \
+    -t ${outdir}/SRR35908235_TRassembly/transcripts.fasta \
+    -m ${models_dir} \
+    -o ${outdir}/SRR35908235_ToxCodAn \
+    -c ${SLURM_CPUS_PER_TASK}
+
+cat ${outdir}/SRR35908235_ToxCodAn/SRR35908235_Toxins_cds_RedundancyFiltered.fasta ${outdir}/SRR35908235_ToxCodAn/SRR35908235_PutativeToxins_cds_SPfiltered.fasta > ${outdir}/G_ussuriensis_VG_toxins.toxcodan.fasta
+
+echo "ToxCodAn completed successfully."
+echo "The output files are located in ${outdir}/SRR35908235_ToxCodAn"
+ls ${outdir}/SRR35908235_ToxCodAn   
